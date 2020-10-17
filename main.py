85d928350424d0a79aef9c517d57856beae3877c
misterwhy <weezixx@hotmail.com>
2020-10-17 22:38:45 +0200

    coucou

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ soup = BeautifulSoup(page.content,'html.parser')
 
 soup = str(soup)
 
-url = re.findall("https://rf.proxycast.org+\/+[A-Za-z0-9_-]{1,100}\/[A-Za-z0-9_-]{1,}\.[A-Za-z0-9_-]{1,}\.[A-Za-z0-9_-]{1,}\.mp3", soup)
+url = re.findall("http://rf.proxycast.org+\/+[A-Za-z0-9_-]{1,100}\/[A-Za-z0-9_-]{1,}\.[A-Za-z0-9_-]{1,}\.[A-Za-z0-9_-]{1,}\.mp3", soup)
 
-print(len(url))
+#http://rf.proxycast.org/5dda516c-eb56-4632-832b-9221d203b030/13957-17.10.2020-ITEMA_22457933-2020C22785E0031.mp3
+
+#<div class="h4 titre-0">Louis Lachenal ou la résurrection d’un alpiniste</div>
+
+titre = re.findall("h4 titre-[0-9]{1,}\"+\>+[A-Za-z0-9'’\-\ \-\'\"\ê\é\è\à\&\ù\ç,()«»]{1,}",soup)
+
+
+print(titre)
